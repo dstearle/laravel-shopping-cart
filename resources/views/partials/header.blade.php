3343e1f92feddd2d221ca985ab4ceec4939e7ed2
dstearle <dallas.st.earle@gmail.com>
2020-05-27 22:00:53 -0700

    Cleaned up header.blade.php with comments and indentation

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,28 +1,51 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
-  <a class="navbar-brand text-white" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link text-white" href="#">
-          <i class="fas fa-shopping-cart"></i> Shopping Cart
-        </a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-user"></i> User Account
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
+	{{-- Brand --}}
+	<a class="navbar-brand text-white" href="#">Navbar</a>
 
-  </div>
+	{{-- Collapse Toggle --}}
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+		<span class="navbar-toggler-icon"></span>
+
+	</button>
+
+	{{-- Collapse Content --}}
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+		<ul class="navbar-nav ml-auto">
+
+			{{-- Shopping Cart --}}
+			<li class="nav-item">
+				<a class="nav-link text-white" href="#">
+					<i class="fas fa-shopping-cart"></i> Shopping Cart
+				</a>
+			</li>
+
+			{{-- User Account --}}
+			<li class="nav-item dropdown">
+
+				<a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<i class="fas fa-user"></i> User Account
+				</a>
+
+				{{-- Dropdown --}}
+				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+					<a class="dropdown-item" href="#">Action</a>
+
+					<a class="dropdown-item" href="#">Another action</a>
+
+					<div class="dropdown-divider"></div>
+
+					<a class="dropdown-item" href="#">Something else here</a>
+					
+				</div>
+
+			</li>
+
+		</ul>
+
+	</div>
+
 </nav>
