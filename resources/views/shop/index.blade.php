805e6a95c89555d7fccc7191118b77a074beaa04
dstearle <dallas.st.earle@gmail.com>
2020-05-28 17:02:15 -0700

    Added in a responsive image for the new card component

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -9,17 +9,38 @@
 @section('content')
 
 <div class="row">
-    
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
 
-  </div>
+	<div class="col-sm-6">
+
+		<div class="card">
+
+			<div class="card-body">
+
+				<div class="row">
+
+					<img 
+						class="img-responsive mx-auto" 
+						style="max-height: 150px"
+						src="https://images-na.ssl-images-amazon.com/images/I/61GMn0FD3IL._SX361_BO1,204,203,200_.jpg" 
+						alt="Card image cap"
+					>
+					
+				</div>
+
+				<h5 class="card-title">Product Title</h5>
+
+				<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates a odit nemo sapiente. Eos, quo.</p>
+
+				<div>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
 
 @endsection
