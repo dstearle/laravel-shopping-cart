7bbcfed80ae98ca057478ac2a4df83547fb0d479
dstearle <dallas.st.earle@gmail.com>
2020-05-29 17:37:01 -0700

    Testing out how multiple rows will look

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -8,7 +8,130 @@
 
 @section('content')
 
-<div class="row">
+{{-- Row 1 --}}
+<div class="row my-3">
+
+	{{-- Item 1 --}}
+	<div class="col-sm-6 col-md-4">
+
+		<div class="card">
+
+			<div class="card-body">
+
+				{{-- Image --}}
+				<div class="row thumbnail">
+
+					<img 
+						class="img-responsive mx-auto" 
+						src="https://images-na.ssl-images-amazon.com/images/I/61GMn0FD3IL._SX361_BO1,204,203,200_.jpg" 
+						alt="Card image cap"
+					>
+					
+				</div>
+
+				{{-- Title --}}
+				<h5 class="card-title">Product Title</h5>
+
+				{{-- Description --}}
+				<p class="card-text description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates a odit nemo sapiente. Eos, quo.</p>
+
+				{{-- Price & Add To Cart Button --}}
+				<div class="row px-3">
+
+					<div class="col-6 my-auto price">$12</div>
+
+					<a href="#" class="btn btn-primary bg-success col-6">Add To Cart</a>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	{{-- Item 3 --}}
+	<div class="col-sm-6 col-md-4">
+
+		<div class="card">
+
+			<div class="card-body">
+
+				{{-- Image --}}
+				<div class="row thumbnail">
+
+					<img 
+						class="img-responsive mx-auto" 
+						src="https://images-na.ssl-images-amazon.com/images/I/61GMn0FD3IL._SX361_BO1,204,203,200_.jpg" 
+						alt="Card image cap"
+					>
+					
+				</div>
+
+				{{-- Title --}}
+				<h5 class="card-title">Product Title</h5>
+
+				{{-- Description --}}
+				<p class="card-text description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates a odit nemo sapiente. Eos, quo.</p>
+
+				{{-- Price & Add To Cart Button --}}
+				<div class="row px-3">
+
+					<div class="col-6 my-auto price">$12</div>
+
+					<a href="#" class="btn btn-primary bg-success col-6">Add To Cart</a>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	{{-- Item 2 --}}
+	<div class="col-sm-6 col-md-4">
+
+		<div class="card">
+
+			<div class="card-body">
+
+				{{-- Image --}}
+				<div class="row thumbnail">
+
+					<img 
+						class="img-responsive mx-auto" 
+						src="https://images-na.ssl-images-amazon.com/images/I/61GMn0FD3IL._SX361_BO1,204,203,200_.jpg" 
+						alt="Card image cap"
+					>
+					
+				</div>
+
+				{{-- Title --}}
+				<h5 class="card-title">Product Title</h5>
+
+				{{-- Description --}}
+				<p class="card-text description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates a odit nemo sapiente. Eos, quo.</p>
+
+				{{-- Price & Add To Cart Button --}}
+				<div class="row px-3">
+
+					<div class="col-6 my-auto price">$12</div>
+
+					<a href="#" class="btn btn-primary bg-success col-6">Add To Cart</a>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</div>
+
+{{-- Row 1 --}}
+<div class="row my-3">
 
 	{{-- Item 1 --}}
 	<div class="col-sm-6 col-md-4">
