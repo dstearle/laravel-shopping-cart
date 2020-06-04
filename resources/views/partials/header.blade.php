f00f9f48bf0b8200d1d0c9401be73d36bba91f5f
dstearle <dallas.st.earle@gmail.com>
2020-06-03 21:52:40 -0700

    Added route to navbar so user can access sign in page

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -34,7 +34,7 @@
 
 					<a class="dropdown-item" href="{{ route('user.signup') }}">Sign Up</a>
 
-					<a class="dropdown-item" href="#">Another action</a>
+					<a class="dropdown-item" href="{{ route('user.signin') }}">Sign In</a>
 
 					<div class="dropdown-divider"></div>
 
