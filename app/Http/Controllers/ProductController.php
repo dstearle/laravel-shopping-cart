194eb4b5b120440c7b8667603982a01eec829866
dstearle <dallas.st.earle@gmail.com>
2020-06-07 14:26:23 -0700

    Updated getAddToCart with method for adding item to cart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 Use App\Product;
 use Illuminate\Http\Request;
+Use session;
 
 class ProductController extends Controller
 {
-    //
+    // Getter for products page
     public function getIndex() {
 
         $products = Product::all();
@@ -15,10 +16,22 @@ class ProductController extends Controller
 
     }
 
-    //
+    // Getter for adding a new item to your cart
     public function getAddToCart(Request $request, $id) {
 
         $product = Product::find($id);
+        // Oldcart if it exists, null if not applicable
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        // The new cart with previous oldCarts contents
+        $cart = new Cart($oldCart);
+        // Adds the new item to the cart
+        $cart->add($product, $product->id);
+
+        // The session for your shopping cart
+        $request->session()->put('cart', $cart);
+        dd($request->session()->get('cart'));
+        // Redirects back to product index
+        return redirect()->route('product.index');
 
     }
 
