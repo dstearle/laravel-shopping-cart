380efcf0dfb3788064bf4f113c00499fbb19af3f
dstearle <dallas.st.earle@gmail.com>
2020-06-29 09:55:10 -0700

    Began working on an orders list for user profile view

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,10 +4,31 @@
 
     <div class="row">
 
-        <div class="col-md-4 col-md-offset-4">
+        <div class="col-md-8 offset-md-2">
 
             {{-- Title --}}
             <h1>Profile</h1>
+
+            <hr>
+
+            {{-- Orders --}}
+            <h2>Your Orders</h2>
+
+            <div class="card text-center">
+
+                <div class="card-body">
+
+                    <ul class="list-group">
+
+                        <li class="list-group-item">First item</li>
+                        
+                    </ul>
+
+                </div>
+
+                <div class="card-footer text-muted">2 days ago</div>
+
+            </div>
             
         </div>
 
