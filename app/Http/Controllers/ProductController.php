ad939853a0170c92767f72ced9c8f5a2504708d3
dstearle <dallas.st.earle@gmail.com>
2020-06-30 15:44:01 -0700

    Created update for reduced cart inventory

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -43,6 +43,23 @@ class ProductController extends Controller
 
     }
 
+    // Getter for updated cart
+    public function getReduceByOne($id) {
+
+        // Oldcart if it exists, null if not applicable
+        $oldCart = Session::has('cart') ? Session::get('cart') : null;
+        // The new cart with previous oldCarts contents
+        $cart = new Cart($oldCart);
+        // Reduces the cart inventory by one
+        $cart->reduceByOne($id);
+
+        // User session
+        Session::put('cart', $cart);
+
+        return redirect()->route('product.shopping-cart');
+
+    }
+
     // Getter for the shopping cart
     public function getCart() {
 
